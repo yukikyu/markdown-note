4f077c0e37ff4791b94ae38c4c84aa3d9304dfdf
zhouyi <zhouy@xi-data.com>
2022-06-09 15:50:42 +0800

    Bytemd初始搭建

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Bytemd from './components/Bytemd.vue'
+</script>
+
+<template>
+  <Bytemd />
+</template>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+</style>
